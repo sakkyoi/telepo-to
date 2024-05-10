f2bdc79d6254fdf2f125ffeda7e476a47954737f
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2024-05-10 14:26:01 +0800

    temporary upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Peer } from 'peerjs';
+import { FormsModule } from "@angular/forms";
+import { ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'telepo-to';
+  peer: Peer;
+
+  @Input() theme: boolean = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+  @ViewChild('welcomeModal') welcomeModal: ElementRef | undefined;
+
+  constructor() {
+
+    this.peer = new Peer({
+      host: 'localhost',
+      port: 9000,
+    });
+
+    this.peer.on('open', (id) => {
+      console.log('My peer ID is: ' + id);
+    });
+
+    this.peer.on('connection', (conn) => {
+      conn.on('data', (data: any) => {
+        console.log(data);
+      });
+    });
+  }
+
+  ngAfterViewInit() {
+    // this.welcomeModal?.nativeElement.showModal();
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["light", "dark"],
+  }
+}
+
