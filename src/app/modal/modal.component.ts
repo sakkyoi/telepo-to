6a725b82664e7b0fb5d3f940fc05308a8fb8dbb4
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2024-05-14 11:30:24 +0800

    init: modal component

---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, ElementRef, ViewChild, Input } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent {
+  @ViewChild('dialog') dialog: ElementRef | undefined;
+  @Input() buttonText: string | undefined;
+  @Input() title: string | undefined;
+  @Input() content: string | undefined;
+}
